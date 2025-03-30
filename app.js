dfe06524097f29da736b1307ea25871e8e60c36a
Mystique <uyooghene01@gmail.com''git config user.name Mystique git config --global user.email uyooghene01@gmail.com>
2025-03-30 22:41:44 +0100

    home

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const app = express();
 const port = 4005; // or any port you prefer
+const path = require('path');
+const Post = require("./models/post");
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
 
+
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Home Route
+// app.get('/', async(req, res) => {
+//   const posts = await Post.find({}); 
+//   res.render('home',{posts});
+// });
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('home');
 });
+
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
