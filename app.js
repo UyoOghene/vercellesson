1de34c019881b78147f6cacc4d515cb1ea9a9c45
Mystique <uyooghene01@gmail.com''git config user.name Mystique git config --global user.email uyooghene01@gmail.com>
2025-04-18 10:38:56 +0100

    prod

--- app.js
+++ app.js
@@ -60,50 +60,50 @@ app.use(express.static(path.join(__dirname, 'public')));
 const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 // for vercel
 
-// const store = MongoDBStore.create({
-//   mongoUrl: mongoURI,
-//   crypto: {
-//     secret: secret
-//   },
-//   touchAfter: 24 * 60 * 60
-// });
+const store = MongoDBStore.create({
+  mongoUrl: mongoURI,
+  crypto: {
+    secret: secret
+  },
+  touchAfter: 24 * 60 * 60
+});
 
-// store.on("error", function(e) {
-//   console.log("SESSION STORE ERROR", e)
-// });
+store.on("error", function(e) {
+  console.log("SESSION STORE ERROR", e)
+});
 
-// const sessionConfig = {
-//   store: store,
-//   name: 'session',
-//   secret: secret,
-//   resave: false,
-//   saveUninitialized: false, 
-//   proxy: true,
-//   cookie: {
-//     httpOnly: true,
-//     secure: true, 
-//     sameSite: 'none', 
-//     domain: process.env.NODE_ENV === 'production' 
-//       ? '.vercellesson.vercel.app' 
-//       : undefined,
-//     maxAge: 1000 * 60 * 60 * 24 * 7
-//   }
-// };
+const sessionConfig = {
+  store: store,
+  name: 'session',
+  secret: secret,
+  resave: false,
+  saveUninitialized: false, 
+  proxy: true,
+  cookie: {
+    httpOnly: true,
+    secure: true, 
+    sameSite: 'none', 
+    domain: process.env.NODE_ENV === 'production' 
+      ? '.vercellesson.vercel.app' 
+      : undefined,
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+};
 
 
 
 // // for Development(localhost)
-const sessionConfig = {
-  secret: 'thisshouldbeabettersecret!',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    maxAge: 1000 * 60 * 60 * 24 * 7
-}
+// const sessionConfig = {
+//   secret: 'thisshouldbeabettersecret!',
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: {
+//     httpOnly: true,
+//     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+//     maxAge: 1000 * 60 * 60 * 24 * 7
+// }
 
-};
+// };
 
 
 app.use(session(sessionConfig));
